d989d4a46a7f889de089f75211f4e1963bed84e3
Certseeds <nanoseedskc@gmail.com>
2019-08-05 19:02:33 +0800

    Runtime:4ms Memory Usage:8.3MB

---
+++ leetcode_cpp/leetcode_1108.cpp
@@ -0,0 +1,42 @@
+#include <string>
+#include <fstream>
+#include <iostream>
+using namespace std;
+string defangIPaddr(string address);
+int main() {
+	string  temp = "1.1.1.1";
+	cout << temp.length() << endl;//ц╩сп-1
+	cout << defangIPaddr(temp);
+	cin.get();
+	cin.get();
+}
+string defangIPaddr(string address) {
+	short * location = new short[3];
+	string * numbers = new string[4];
+	short i = 0;
+	for (int loc = 0; loc < address.length(); loc++) {
+		if (address[loc] == '.') {
+			location[i] = loc;
+			i++;
+		}
+	}
+	int begin = 0;
+	int end = location[0]; 
+	{
+		numbers[0] = address.substr(0,location[0]);
+		numbers[1] = address.substr(location[0] + 1, location[1] - location[0]-1);
+		numbers[2] = address.substr(location[1] + 1, location[2] - location[1] -1);
+		numbers[3] = address.substr(location[2] + 1);
+
+	}
+	string  temp = "[.]";
+	string willreturned = "";
+
+	for (int i = 0; i < 3; i++) {
+		willreturned= willreturned + numbers[i] + temp;
+	}
+	willreturned += numbers[3];
+	delete[]location;
+	delete[]numbers;
+	return willreturned;
+}
