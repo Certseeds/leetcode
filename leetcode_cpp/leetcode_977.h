22887149d102d7b14021c2eda33987436e011f20
Certseeds <nanoseedskc@gmail.com>
2019-08-12 17:34:32 +0800

    finish leetcode 977 & 657

---
+++ leetcode_cpp/leetcode_977.h
@@ -0,0 +1,22 @@
+#pragma once
+#ifndef _LEETCODE_977_H
+#define _LEETCODE_977_H
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+class Solution977 {
+	public:
+		vector<int> & sortedSquares(vector<int>& A) {
+			for (int i = 0; i < A.size(); i++)
+			{
+				A[i] = A[i] * A[i];
+			}
+			std::sort(A.begin(), A.end());
+			return A;
+		}
+
+	};
+
+#endif
