777d28da9c6a07c0d23469e9e1c82af5e10121ff
Certseeds <nanoseedskc@gmail.com>
2019-08-18 15:54:36 +0800

    finish leetcode 589 / 590, Runtime: 148/ 144ms, Memory: 32.5 / 32.3

---
+++ leetcode_cpp/leetcode_590.h
@@ -0,0 +1,37 @@
+#pragma once
+#ifndef _LEETCODE_590_H
+#define _LEETCODE_590_H
+#include <vector>
+using namespace std;
+
+class Node590 {
+public:
+	int val;
+	vector<Node590*> children;
+	Node590() {}
+	Node590(int _val, vector<Node590*> _children) {
+		val = _val;
+		children = _children;
+	}
+};
+class Solution590 {
+public:
+	vector<int> postorder(Node590* root) {
+		vector <int> vec;
+		real_action(root, vec);
+		return vec;
+	}
+	void real_action(Node590 * root, vector<int> & vec) {
+		if (root == nullptr)
+		{
+			return;
+		}
+		for (int i = 0; i < root->children.size(); i++)
+		{
+			real_action(root->children[i], vec);
+		}
+		vec.push_back(root->val);
+	}
+};
+
+#endif
