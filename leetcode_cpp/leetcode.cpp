cd0577201e93db3b79b419bf56e3f2baabf45b4a
Seeds Nano <nanoseeds@outlook.com>
2020-01-12 20:01:50 +0800

    finish leetcode 1217, Runtime: 0 ms, Memory Usage: 8.7 MB, 100%,100%.

--- leetcode_cpp/leetcode.cpp
+++ leetcode_cpp/leetcode.cpp
@@ -47,6 +47,7 @@
 #include "leetcode_1030.h"
 #include "leetcode_1185.h"
 #include "leetcode_806.h"
+#include "leetcode_1217.h"
 using namespace std;
 void main_1108();
 void main_771();
@@ -93,6 +94,7 @@ void main_893();
 void main_1030();
 void main_1185();
 void main_806();
+void main_1217();
 int main() {
 	//main_1108();
 	//main_771();
@@ -138,12 +140,16 @@ int main() {
 	//main_893();
 	//main_1030();
 	//main_1185();
-	main_806();
+	//main_806();
+	main_1217();
 	cout << "main end"<<endl;
 	cin.get();
 	cin.get();
 	
 return 0;
+}void main_1217() {
+	Solution1217 sol1217;
+	sol1217.test();
 }void main_806() {
 	Solution806 sol806;
 	sol806.test();
