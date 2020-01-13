874f2234d05254fedf4da5f1f9c85264172dd05e
Seeds Nano <nanoseeds@outlook.com>
2020-01-13 11:25:42 +0800

    update leetcode 1282, Runtime: 24 ms, Memory Usage: 11.3 MB, 86.52%,100%.

--- leetcode_cpp/leetcode_1282.h
+++ leetcode_cpp/leetcode_1282.h
@@ -14,26 +14,38 @@ Also, it is guaranteed that there exists at least one solution.
 class Solution1282 {
 public:
 	vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
+	    unordered_map<int, vector<int>> nums;
+	    vector < vector<int>> willreturn;
+	    for (int i = 0; i < groupSizes.size(); i++) {
+		    nums[groupSizes[i]].push_back(i);
+	    }
+	    for (auto iter = nums.begin(); iter != nums.end(); iter++) {
+		    for (int i = 0; i < (*iter).second.size() / (*iter).first; i++) {
+		    	vector<int> temp;
+			    for (int j = 0; j < (*iter).first; j++) {
+				    temp.push_back((*iter).second[i * (*iter).first + j]);
+			    }
+			    willreturn.push_back(temp);
+		    }
+	    }
+	    return willreturn;
+    }
+	vector<vector<int>> groupThePeople2(vector<int>& groupSizes) {
 		unordered_map<int,vector<int>> nums;
 		vector <vector<int>> willreturn;
 		for (int i = 0; i < groupSizes.size(); i++) {
-			if (nums[groupSizes[i]].size() > groupSizes[i]) {
-			}
 			nums[groupSizes[i]].push_back(i);
-		}
-		for (auto iter = nums.begin(); iter != nums.end(); iter++) {
-			for (int i = 0; i < (*iter).second.size()/ (*iter).first; i++) {
-				vector<int> temp;
-				for (int j = 0; j <(*iter).first; j++) {
-					temp.push_back((*iter).second[i * (*iter).first + j]);
-				}
-				willreturn.push_back(temp);
+			if (nums[groupSizes[i]].size() == groupSizes[i]) {
+				willreturn.push_back(nums[groupSizes[i]]);
+				nums[groupSizes[i]].clear();
 			}
 		}
 		return willreturn;
 	}
+
 	void test() {
 		vector<int> vec1 = { 3,3,3,3,3,1,3 };
+		vector<int> vec2 = { 2,1,3,3,3,2 };
 		vector<vector<int>> return1 = groupThePeople(vec1);
 		for (auto i : return1){
 			for (auto j : i) {
@@ -41,6 +53,28 @@ public:
 			}
 			cout << endl;
 		}
+		vector<vector<int>> return2 = groupThePeople(vec2);
+		for (auto i : return2) {
+			for (auto j : i) {
+				cout << j << " ";
+			}
+			cout << endl;
+		}
+		vector<vector<int>> return3 = groupThePeople2(vec1);
+		for (auto i : return3) {
+			for (auto j : i) {
+				cout << j << " ";
+			}
+			cout << endl;
+		}
+		vector<vector<int>> return4 = groupThePeople2(vec2);
+		for (auto i : return4) {
+			for (auto j : i) {
+				cout << j << " ";
+			}
+			cout << endl;
+		}
+
 	}
 };
 
