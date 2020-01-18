eac031c84587644c779f4d68f4e85e379e23c11e
Seeds Nano <nanoseeds@outlook.com>
2020-01-18 16:48:23 +0800

    last finish is 1013.

--- leetcode_cpp/leetcode_1013.h
+++ leetcode_cpp/leetcode_1013.h
@@ -14,6 +14,7 @@ we can partition the array if we can find indexes i+1 < j with
 (A[0] + A[1] + ... + A[i] ==
 A[i+1] + A[i+2] + ... + A[j-1] ==
 A[j] + A[j-1] + ... + A[A.length - 1])
+can be improve
 */
 class Solution1013 {
 public:
