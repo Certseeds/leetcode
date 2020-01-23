1ecf657b6b2d697b9ea9d749e0bcdf283728efa6
Seeds Nano <nanoseeds@outlook.com>
2020-01-23 16:39:45 +0800

    finish leetcode 119 Runtime: 8 ms, Memory Usage: 8.4 MB, 61.00%,90.32%.

--- leetcode_cpp/leetcode.cpp
+++ leetcode_cpp/leetcode.cpp
@@ -234,7 +234,7 @@
 
 #define TO_STRING(str) _TO_STRING(str)
 #define _TO_STRING(str) #str
-#define leetcode_NUM 27
+#define leetcode_NUM 119
 #include TO_STRING(leetcode_include)
 
 
--- leetcode_cpp/leetcode_119.h
+++ leetcode_cpp/leetcode_119.h
@@ -1,16 +1,36 @@
-
 #pragma once
-#ifndef _LEETCODE_119_H
-#define _LEETCODE_119_H
+#ifndef _LEETCODE_119_H_
+#define _LEETCODE_119_H_
 #include <vector>
-#include <array>
-#include <string>
 #include <algorithm>
 using namespace std;
+/*
+Given a non-negative index k where k ¡Ü 33, 
+return the kth index row of the Pascal's triangle.
+
+Note that the row index starts from 0.
+*/
 class Solution119 {
 public:
-  void test() {
-  }
+    vector<int> getRow(int rowIndex) {
+        vector<int> willreturn(rowIndex + 1, 1);
+        for (int i = 1; i <= rowIndex; i++) {
+            for (int j = i-1; j > 0; j--) {
+                willreturn[j] += willreturn[j - 1];
+            }
+        }
+        return willreturn;
+    }
+    void test() {
+        vector<int> out1 = getRow(3);
+        for (int i : out1) {
+            cout << i << " ";
+        }cout << endl;
+    }
 };
-
+/*
+Runtime: 8 ms,
+Memory Usage: 8.4 MB,
+61.00%,90.32%.
+*/
 #endif
