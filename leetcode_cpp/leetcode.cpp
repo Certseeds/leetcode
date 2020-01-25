f237b04cc18d6b1885f6d568bfc90f63a258ec66
Seeds Nano <nanoseeds@outlook.com>
2020-01-25 11:04:41 +0800

    finish leetcode 35, Runtime: 8 ms, Memory Usage: 8.8 MB, 59.37%,100%.

--- leetcode_cpp/leetcode.cpp
+++ leetcode_cpp/leetcode.cpp
@@ -234,7 +234,7 @@
 
 #define TO_STRING(str) _TO_STRING(str)
 #define _TO_STRING(str) #str
-#define leetcode_NUM 69
+#define leetcode_NUM 35
 #include TO_STRING(leetcode_include)
 
 
--- leetcode_cpp/leetcode_35.h
+++ leetcode_cpp/leetcode_35.h
@@ -1,16 +1,34 @@
-
 #pragma once
-#ifndef _LEETCODE_35_H
-#define _LEETCODE_35_H
+#ifndef _LEETCODE_35_H_
+#define _LEETCODE_35_H_
 #include <vector>
 #include <array>
 #include <string>
 #include <algorithm>
 using namespace std;
+/*
+Given a sorted array and a target value,
+return the index if the target is found.
+If not,
+return the index where it would be if it were inserted in order.
+*/
 class Solution35 {
 public:
-  void test() {
-  }
+    int searchInsert(vector<int>& nums, int target) {
+        auto diff = std::lower_bound(nums.begin(),nums.end(), target);
+        return distance(nums.begin(),diff);
+    }
+    void test() {
+        vector<int> vec1 = { 1,3,5,6 };
+        cout << searchInsert(vec1, 5) << endl;
+        cout << searchInsert(vec1, 2) << endl;
+        cout << searchInsert(vec1, 7) << endl;
+        cout << searchInsert(vec1, 0) << endl;
+    }
 };
-
+/*
+Runtime: 8 ms,
+Memory Usage: 8.8 MB,
+59.37%,100%.
+*/
 #endif
