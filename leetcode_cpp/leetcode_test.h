08323f5af8a2079f8c8fc8fdbc3a89469c8430b2
Certseeds <nanoseedskc@gmail.com>
2020-01-30 20:11:58 +0800

    finish leetcode 605, Runtime: 12 ms, Memory Usage: 10.1 MB, 96.93%,100%.

--- leetcode_cpp/leetcode_test.h
+++ leetcode_cpp/leetcode_test.h
@@ -12,7 +12,25 @@ using namespace std;
 class Solutiontest {
 public:
     void test() {
-        test_NUM(100);
+        //test_NUM(100);
+        cout << test_UB_1(5) << endl;
+    }
+    int  test_UB_1(int i) {
+        int j = -2;
+        switch (i) {
+        case 0:   do {
+        case 1:   i++;
+        case 2:  if (j += ++i < 11)
+        case 3:  continue;
+        case 4:  if (i % 4) {
+        case 5:  j = i / 2;
+        case 6:   break;
+        case 7: ;}
+        case 8:   i += j / 10;
+        case 9:;} while (i < 10);
+        default: j += i;
+        }    
+        return j;
     }
     void test_100() {
         cout << "100 is running" << endl;
