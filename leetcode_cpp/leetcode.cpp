de837c40fff70e45a9f3c5a49e0bccedcf37541c
Certseeds <nanoseedskc@gmail.com>
2020-02-05 10:02:36 +0800

    finish leetcode 15, Runtime: 224 ms, Memory Usage: 34.5 MB, 16.98%,5.30%.

--- leetcode_cpp/leetcode.cpp
+++ leetcode_cpp/leetcode.cpp
@@ -5,7 +5,7 @@
 #include "assert.h"
 #include "leetcode_macro.h"
 
-#define leetcode_NUM 14
+#define leetcode_NUM 15
 #include TO_STRING(leetcode_include)
 using namespace std;
 void function_head();
--- leetcode_cpp/leetcode_hash.h
+++ leetcode_cpp/leetcode_hash.h
@@ -9,5 +9,25 @@ namespace std {
             return std::hash<T>()(p.first) ^ std::hash<U>()(p.second);
         }
     };
+    template<typename X, typename Y,typename Z>
+    class hash < tuple <X,Y,Z >> {
+    public:
+        size_t operator()(const tuple <X, Y, Z >& p) const {
+            return (
+            std::hash<X>()(std::get<0>(p)) ^
+            std::hash<Y>()(std::get<1>(p)) ^ 
+            std::hash<Z>()(std::get<2>(p)) );
+        }
+    }; 
+    class int32_vector_hasher {
+    public:
+        std::size_t operator()(std::vector<int32_t> const& vec) const {
+            std::size_t ret = 0;
+            for (auto& i : vec) {
+                ret ^= std::hash<int32_t>()(i);
+            }
+            return ret;
+        }
+    };
 }
 #endif
