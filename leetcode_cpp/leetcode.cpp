d16b429e62ed96c5b7bad63e0beb884f01482b74
Certseeds <nanoseedskc@gmail.com>
2020-02-05 10:55:41 +0800

    finish leetcode 17, Runtime: 0 ms, Memory Usage: 9.8 MB, 100%,5.71%.

--- leetcode_cpp/leetcode.cpp
+++ leetcode_cpp/leetcode.cpp
@@ -5,7 +5,7 @@
 #include "assert.h"
 #include "leetcode_macro.h"
 
-#define leetcode_NUM 16
+#define leetcode_NUM 17
 #include TO_STRING(leetcode_include)
 using namespace std;
 void function_head();
