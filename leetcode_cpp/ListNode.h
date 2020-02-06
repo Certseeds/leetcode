ef202c7154d809362f9e3fab3d0640d2d7e651f2
Certseeds <nanoseedskc@gmail.com>
2020-02-06 10:15:22 +0800

    finish leetcode 19, Runtime: 4 ms, Memory Usage: 8.9 MB, 90.40%,5.26%.

--- leetcode_cpp/ListNode.h
+++ leetcode_cpp/ListNode.h
@@ -1,9 +1,47 @@
 #pragma once
 #ifndef _ListNode_H_
 #define _ListNode_H_
+#include <vector>
+/*
+ * @Github: https://github.com/Certseeds
+ * @Organization: SUSTech
+ * @Author: nanoseeds
+ * @Date: 2020-02-02 21:01:26
+ * @LastEditors  : nanoseeds
+ * @LastEditTime : 2020-02-06 09:30:22
+ */
+using std::vector;
+const int No = -100000;
 struct ListNode {
     int val;
     ListNode* next;
-    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x);
+    ListNode(const ListNode& obj);
+    ~ListNode();
+    static vector<ListNode*> numToList(vector<int> nums);
 };
+ListNode::ListNode(int x) {
+    this->val = x;
+    this->next = nullptr;
+}
+ListNode::ListNode(const ListNode& obj) {
+    this->val = obj.val;
+    this->next = obj.next;
+}
+ListNode::~ListNode() {
+    this->val = 0;
+    this->next = 0;
+}
+vector<ListNode*> ListNode::numToList(vector<int> nums) {
+    vector<ListNode*> will_return(nums.size(), nullptr);
+    for (int i = 0; i < nums.size(); i++) {
+        will_return[i] =
+            (nums[i] != No) ?
+            new ListNode(nums[i]) : nullptr;
+    }
+    for(int i = 0; i < nums.size()-1; i++) {
+        will_return[i]->next = will_return[i + 1];
+    }
+    return will_return;
+}
 #endif
