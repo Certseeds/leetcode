68406f7a1a258dbc8e3267c9d93ed95cc32dac3b
Certseeds <nanoseedskc@gmail.com>
2020-02-15 09:26:54 +0800

    finish leetcode 784, Runtime: 4 ms, Memory Usage: 11.1 MB, 99.36%,100%.

--- leetcode_cpp/leetcode.cpp
+++ leetcode_cpp/leetcode.cpp
@@ -4,7 +4,7 @@
 #include "assert.h"
 #include "leetcode_macro.h"
 #include "leetcode_helper.hpp"
-#define leetcode_NUM 1104
+#define leetcode_NUM 784
 #include TO_STRING(leetcode_include)
 //using namespace std;
 using std::ios;
